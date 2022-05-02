5a45fbdc12b1768596467d20c666f73f0b227324
xetnal <santiago.deaguirre@gmail.com>
2022-05-02 21:42:12 +0000

    fixed fetch

--- src/js/store/appContext.js
+++ src/js/store/appContext.js
@@ -22,7 +22,7 @@ const injectContext = PassedComponent => {
 		);
 		useEffect(() => {
 			state.actions.getPlanets();
-			// state.actions.getVehicles();
+			state.actions.getVehicles();
 		}, []);
 
 		// The initial value for the context is not null anymore, but the current state of this component,
