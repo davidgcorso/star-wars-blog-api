7843c81328158df30f9b9786b5e6f4e2beaf29dd
xetnal <santiago.deaguirre@gmail.com>
2022-05-02 17:02:23 +0000

    fixed aesthetic

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -3,14 +3,33 @@ const getState = ({ getStore, getActions, setStore }) => {
 		store: {
 			planets: [],
 			vehicles: [],
-			characters: []
+			characters: [],
+			planetsData: []
 		},
 		actions: {
 			getPlanets: () => {
 				fetch('https://www.swapi.tech/api/planets')
 					.then((response) => response.json())
 					.then((data) => setStore( { planets: data }))
+					.then( async data => {
+						const { results } = data;
+						results.forEach(async (planet, index) => {
+							const { attributes } = planet;
+							attributes.forEach(async (planet, i) => {
+								const resp = await fetch(planet)
+								const info = await resp.json();
+								data.results[index].attributes[i] = info
+								setStore( {planets: data})
+							})
+							
+						});
+					})
+						
+					
 			
+			},
+			getPlanetProps: () => {
+
 			}
 		},
 	}
