195dcb8ab2cd0993581d5a9be18e2346d1550c3a
xetnal <santiago.deaguirre@gmail.com>
2022-05-03 16:48:24 +0000

    added not found

--- src/js/layout.js
+++ src/js/layout.js
@@ -6,50 +6,55 @@ import { Home } from "./views/home";
 
 import { Planets } from "./views/planets";
 import injectContext from "./store/appContext";
-import 'animate.css';
+import "animate.css";
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
 import { PlanetDetails } from "./views/planetdetails";
 
 import { People } from "./views/people";
 
-
 //create your first component
 const Layout = () => {
-	//the basename is used when your project is published in a subdirectory and not in the root of the domain
-	// you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
-	const basename = process.env.BASENAME || "";
+  //the basename is used when your project is published in a subdirectory and not in the root of the domain
+  // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
+  const basename = process.env.BASENAME || "";
 
-	return (
-		<div>
-			<BrowserRouter basename={basename}>
-				<ScrollToTop>
-					<Navbar />
-					<Switch>
-						<Route exact path="/">
-							<Home />
-						</Route>
-						<Route exact path="/people">
-							<People />
-						</Route>
-						<Route exact path="/planets">
-							<Planets />
-						</Route>
-						<Route exact path="/vehicles">
-							<Vehicles />
-						</Route>
-						<Route exact path={`/planets/:id`}>
-							<PlanetDetails />
-						</Route>
-						<Route>
-							<h1>Not found!</h1>
-						</Route>
-					</Switch>
-					<Footer />
-				</ScrollToTop>
-			</BrowserRouter>
-		</div>
-	);
+  return (
+    <div>
+      <BrowserRouter basename={basename}>
+        <ScrollToTop>
+          <Navbar />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/people">
+              <People />
+            </Route>
+            <Route exact path="/planets">
+              <Planets />
+            </Route>
+            <Route exact path="/vehicles">
+              <Vehicles />
+            </Route>
+            <Route exact path={`/planets/:id`}>
+              <PlanetDetails />
+            </Route>
+            <Route>
+              <h1 className="title text-center">Not found!</h1>
+              <div className="itemDescription">
+                <p className="paragraph card">
+                  Our ewoks are working hard on finding the page you wanted, but
+                  failed. <br /> Dont worry though, we got rid of them...
+                </p>
+              </div>
+            </Route>
+          </Switch>
+          <Footer />
+        </ScrollToTop>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default injectContext(Layout);
--- src/js/views/planetdetails.js
+++ src/js/views/planetdetails.js
@@ -23,7 +23,7 @@ export const PlanetDetails = () => {
         {store.singlePlanet?.result.properties.name}
       </h1>
       <div className="itemDescription">
-          <p className="card paragraph">{name} is a planet with a population of {population} sentient habitants. <br /> With the terrain being mostly {terrain} and other tiny variations, the planet has a diameter of {diameter}, an orbital period of {orbitalPeriod} days and a gravity index of {gravity} units.<br /> The climate on this planet is {climate}, so make sure to plan your journey accordingly.</p>
+          <p className="card paragraph">{name} is a planet with a population of {population} sentient habitants. <br /> With the terrain being mostly {terrain} and other tiny variations, the planet has a diameter of {diameter} kms, an orbital period of {orbitalPeriod} days and a gravity index of {gravity} units.<br /> The climate on this planet is {climate}, so make sure to plan your journey accordingly.</p>
       </div>
       
     </>
